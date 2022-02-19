ebf33bcc56e184895098a530b7204a6f5dbaed0a
Jibbyworks <JDBucog@yahoo.com>
2022-02-19 11:25:27 -0800

    notFound route

--- app.js
+++ app.js
@@ -7,12 +7,16 @@ const tasks = require("./routes/tasks")
 const connectDB = require("./db/connect")
 require("dotenv").config()
 
+const notFound = require("./middleware/notfound")
+
 //middleware
 app.use(express.static("public"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use("/api/v1/tasks", tasks)
+
+app.use(notFound)
 
 const start = async () => {
   try {
